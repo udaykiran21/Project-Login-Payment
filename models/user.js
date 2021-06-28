d675c535a71e897649602d1c3f0be58e3d7fd0cf
B. Uday Kiran <63153044+udaykiran21@users.noreply.github.com>
2021-06-28 14:34:21 +0530

    This is the user schema

---
+++ models/user.js
@@ -0,0 +1,17 @@
+// importing modules
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+  
+  
+var UserSchema = new Schema({   
+   
+    username : String,
+    pro: {type: Boolean, default: false}
+});
+  
+// plugin for passport-local-mongoose
+UserSchema.plugin(passportLocalMongoose);
+  
+// export userschema
+ module.exports = mongoose.model("User", UserSchema);
